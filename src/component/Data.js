12f6e6bfbb9267a37105934b8c5e6a25767b13b4
neharadialcode <neharadialcode@gmail.com>
2022-09-29 17:22:45 +0530

    id changed

--- src/component/Data.js
+++ src/component/Data.js
@@ -1,5 +1,13 @@
 export const data = [
   {
+    title: "Demon Deck",
+    createdAt: "26-09-2022",
+    dueOn: "27-09-2022",
+    isCompleted: true,
+    isNew: false,
+    id: 3,
+  },
+  {
     title: "BS",
     createdAt: "24-09-2022",
     dueOn: "28-09-2022",
@@ -16,20 +24,21 @@ export const data = [
     id: 2,
   },
   {
-    title: "Demon Deck",
-    createdAt: "26-09-2022",
-    dueOn: "27-09-2022",
-    isCompleted: true,
-    isNew: false,
-    id: 3,
+    title: "Demon yellow",
+    createdAt: "4-10-2022",
+    dueOn: "5-10-2022",
+    isCompleted: false,
+    isNew: true,
+    id: 8,
   },
+
   {
     title: "ABC",
     createdAt: "24-09-2022",
     dueOn: "30-09-2022",
     isCompleted: false,
     isNew: true,
-    id: 4,
+    id: 6,
   },
   {
     title: "BSnew",
@@ -37,7 +46,7 @@ export const data = [
     dueOn: "30-09-2022",
     isCompleted: true,
     isNew: false,
-    id: 1,
+    id: 5,
   },
   {
     title: "Slinky Winky full",
@@ -45,15 +54,7 @@ export const data = [
     dueOn: "1-10-2022",
     isCompleted: true,
     isNew: false,
-    id: 2,
-  },
-  {
-    title: "Demon yellow",
-    createdAt: "4-10-2022",
-    dueOn: "5-10-2022",
-    isCompleted: false,
-    isNew: true,
-    id: 3,
+    id: 7,
   },
   {
     title: "XYZ",
